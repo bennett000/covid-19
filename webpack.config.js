0f90d0c0241d7e03f16f09d5eb3241e0f425e791
Michael J. Bennett <dev@michaeljbennett.info>
2020-03-15 12:51:04 -0400

    First steps towards a "web framework" type of approach

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const TARGET = './docs';
+const production = process.env.NODE_ENV === 'debug' ? false : true;
+
+module.exports = {
+  devServer: {},
+  devtool: production ? 'hidden-source-map' : 'eval-cheap-source-map',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, TARGET),
+    filename: 'adventure-automator.js',
+  },
+  mode: production ? 'production' : 'development',
+  module: {
+    rules: [{ test: /\.tsx?$/, use: 'ts-loader' }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CopyPlugin([{ from: 'assets', to: 'docs' }]),
+  ],
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+  },
+};
